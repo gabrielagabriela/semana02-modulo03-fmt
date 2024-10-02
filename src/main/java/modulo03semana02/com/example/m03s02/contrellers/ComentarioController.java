9b0732522d469150281760a594b98c2e98d402dc
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-10-02 13:16:02 -0300

    Exercicio 06

--- src/main/java/modulo03semana02/com/example/m03s02/contrellers/ComentarioController.java
+++ src/main/java/modulo03semana02/com/example/m03s02/contrellers/ComentarioController.java
@@ -1,8 +1,17 @@
 package modulo03semana02.com.example.m03s02.contrellers;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import modulo03semana02.com.example.m03s02.entities.Comentario;
+import modulo03semana02.com.example.m03s02.entities.Sugestao;
 import modulo03semana02.com.example.m03s02.services.ComentarioService;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("comentarios")
+@Tag(name = "Comentário - CRUD", description = "CRUD de comentários")
 public class ComentarioController {
 
     private final ComentarioService service;
@@ -19,16 +29,47 @@ public class ComentarioController {
         this.service = service;
     }
 
+    @Operation(description = "Buscar todos os comentários cadastrados", summary = "buscar todos")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Comentários encontrados",
+                    useReturnTypeSchema = true
+            ),
+    })
     @GetMapping
     public ResponseEntity<List<Comentario>> buscarTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(service.buscarTodos());
     }
 
+
+    @Operation(description = "Buscar comentários pelo ID", summary = "buscar por ID")
     @GetMapping("{id}")
+    @Parameter(description = "Id do comentário")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Encontrado",
+                    content = @Content(schema = @Schema(implementation = Comentario.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Não encontrado",
+                    content = @Content(schema = @Schema(implementation = ChangeSetPersister.NotFoundException.class))
+            )
+    })
     public ResponseEntity<Comentario> buscarPorId(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(service.buscarPorId(id));
     }
 
+    @Operation(description = "Criação de um comentário", summary = "Criar comentário")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Comentário criado!",
+                    useReturnTypeSchema = true
+            ),
+    })
     @PostMapping
     public ResponseEntity<Comentario> criar(@RequestBody Comentario comentario){
         return ResponseEntity.status(HttpStatus.OK).body(service.criar(comentario));
--- src/main/java/modulo03semana02/com/example/m03s02/contrellers/SugestaoController.java
+++ src/main/java/modulo03semana02/com/example/m03s02/contrellers/SugestaoController.java
@@ -1,8 +1,16 @@
 package modulo03semana02.com.example.m03s02.contrellers;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import modulo03semana02.com.example.m03s02.entities.Sugestao;
 import modulo03semana02.com.example.m03s02.services.SugestaoService;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("sugestoes")
+@Tag(name = "Sugestão - CRUD", description = "CRUD de sugestões")
 public class SugestaoController {
 
     private final SugestaoService service;
@@ -19,18 +28,49 @@ public class SugestaoController {
         this.service = service;
     }
 
+    @Operation(description = "Buscar todas as sugestões cadastradas", summary = "buscar todos")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Sugestões encontradas",
+                    useReturnTypeSchema = true
+            ),
+    })
     @GetMapping
     public ResponseEntity<List<Sugestao>> buscarTodos(){
         return ResponseEntity.status(HttpStatus.OK).body(service.buscarTodos());
     }
 
+    @Operation(description = "Buscar a sugestão pelo ID", summary = "buscar por ID")
     @GetMapping("{id}")
+    @Parameter(description = "Id da sugestão")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Encontrado",
+                    content = @Content(schema = @Schema(implementation = Sugestao.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Não encontrado",
+                    content = @Content(schema = @Schema(implementation = ChangeSetPersister.NotFoundException.class))
+            )
+    })
     public ResponseEntity<Sugestao> buscarPorId(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(service.buscarPorId(id));
     }
 
+    @Operation(description = "Criação de uma sugestão", summary = "Criar sugestão")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Sugestão criada",
+                    useReturnTypeSchema = true
+            ),
+    })
     @PostMapping
     public ResponseEntity<Sugestao> criar(@RequestBody Sugestao sugestao){
         return ResponseEntity.status(HttpStatus.OK).body(service.criar(sugestao));
     }
 }
+
